8fd266ca142fdc19088ee342939ae46ac86e7bbe
xiaoweicheng <2193485092@qq.com>
2019-12-07 11:10:16 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import ComplexButtonDemo from './components/test'
 import './App.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ComplexButtonDemo />
     </div>
   );
 }
